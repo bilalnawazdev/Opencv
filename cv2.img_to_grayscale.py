89f03eecba4a4d95a87a0f11e211af7c51a1b6ce
Bilal <70621618+Bilal-10@users.noreply.github.com>
2020-09-09 12:21:27 +0530

    Update cv2.img_to_grayscale.py

--- cv2.img_to_grayscale.py
+++ cv2.img_to_grayscale.py
@@ -1,8 +1,8 @@
 import cv2
 
-image=cv2.imread(r'C:\Users\bilal\Pictures\Downloaded Wallpapers\lastofus_1.jpg')
+im=cv2.imread(r'C:\Users\bilal\Pictures\Downloaded Wallpapers\lastofus_1.jpg')
 
-gray_scale=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+gray_scale=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
 
 cv2.imshow('Gray scale Image',gray_scale)
 
